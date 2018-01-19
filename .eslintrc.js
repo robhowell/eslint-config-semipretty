65e3642b861caf7c3c8b900e3218a3c2d08972b1
Rob Howell <rob@robhowell.com>
2018-01-19 18:55:26 +0000

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// Based on "Your last ESLint config": https://medium.com/@netczuk/your-last-eslint-config-9e35bace2f99
+
+module.exports = {
+  extends: ['semistandard', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ]
+  },
+};
